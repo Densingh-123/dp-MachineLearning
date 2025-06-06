bad10e4eec1de7de2041d9d33ad50cccf329c7a2
Densingh-123 <ddensingh19@gmail.com>
2025-06-06 12:48:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import pandas as np
 st.title('🤖 Machine Learning App')
 
 st.info("This is a app build Machine Learning Model..")
-df = pd.read_cdv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
 df
